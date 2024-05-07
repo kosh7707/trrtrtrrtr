92bfd89dcd8a75bfd46ae2cf162a2ef0c3c6893a
Kosh <kosh7707@gmail.com>
2024-05-07 11:51:55 +0900

    Item Model 추가 및 Inventory Class 개선, EventHandling 파트 공사중

---
+++ GameServer/Model/Item.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Administrator on 2024-05-07.
+//
+
+#include "Item.h"
---
+++ GameServer/Model/Item.h
@@ -0,0 +1,14 @@
+//
+// Created by Administrator on 2024-05-07.
+//
+
+#ifndef GAMESERVER_ITEM_H
+#define GAMESERVER_ITEM_H
+
+
+class Item {
+
+};
+
+
+#endif //GAMESERVER_ITEM_H
